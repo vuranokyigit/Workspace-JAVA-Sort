e5d40909e7fa6747c524833c8ed71bda7c68821e
VuranokYigit <fyvuranokk@gmail.com>
2023-10-22 15:38:22 +0300

    selectionSortAlg

---
+++ src/main/java/com/example/workspace/WorkspaceApplication.java
@@ -0,0 +1,15 @@
+package com.example.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkspaceApplication.class, args);
+
+
+    }
+
+}
